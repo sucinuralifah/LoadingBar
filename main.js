b36d0622cc894a77a305274bbb59ec6cc4e1c978
sucinuralifah <38427368+sucinuralifah@users.noreply.github.com>
2018-04-17 20:24:08 +0800

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    $('#btnstart').click(function() {
-        $('#loading-isi').animate({width: '100%'}, 10000, 'swing', function() {
-            window.location = 'https://itk.ac.id';
-        });
-    });
-});
